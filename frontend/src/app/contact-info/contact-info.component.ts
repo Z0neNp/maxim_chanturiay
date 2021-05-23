1010cba5526db1a46084f0cd96d6b2fc7f1472de
Maxim Chanturiay <ardorm79@hotmail.co.il>
2021-05-23 19:50:47 +0300

    Contact Info component initialized

---
+++ frontend/src/app/contact-info/contact-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-info',
+  templateUrl: './contact-info.component.html',
+  styleUrls: ['./contact-info.component.sass']
+})
+export class ContactInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
