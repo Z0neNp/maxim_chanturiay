fb56a107957544ca9011af6822dcf3184a283133
Maxim Chanturiay <ardorm79@hotmail.co.il>
2021-05-23 19:37:37 +0300

    Core Review Segments

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { EnterpriseExperienceComponent } from './enterprise-experience/enterprise-experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactInfoComponent,
+    AboutMeComponent,
+    EnterpriseExperienceComponent,
+    PortfolioComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
