ceab5a233fee1a0762c6166db74961f0abcbd6e4
Maxim Chanturiay <ardorm79@hotmail.co.il>
2021-05-30 17:41:02 +0300

    AboutMe component has AbtMeRsmElTitleDirective tests

--- frontend/src/app/about-me/about-me.component.spec.ts
+++ frontend/src/app/about-me/about-me.component.spec.ts
@@ -1,34 +1,65 @@
+import { DebugNode } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AppModule } from '../app.module';
 
 import { AboutMeComponent } from './about-me.component';
+import { AbtMeRsmElTitleDirective } from './rsm-el-title.directive';
 
 describe('AboutMeComponent', () => {
   let component: AboutMeComponent;
   let fixture: ComponentFixture<AboutMeComponent>;
+  let abtMeRsmElTitleDirectiveQueryResponse: DebugNode[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutMeComponent ]
+      declarations: [ AboutMeComponent, AbtMeRsmElTitleDirective],
+      imports: [AppModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutMeComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    component = fixture.componentInstance;
+    abtMeRsmElTitleDirectiveQueryResponse = fixture.debugElement.queryAll(
+      By.directive(AbtMeRsmElTitleDirective)
+    );
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'About Me'`, () => {
+  it("should have as title \"About Me\"", () => {
     expect(component.title).toEqual('About Me');
   });
+  
+  it("should contain the directives", () => {
+    expect(abtMeRsmElTitleDirectiveQueryResponse).toHaveSize(1);
+  });
 
-  it('should render the title', () => {
-    let h1: HTMLElement = fixture.nativeElement.querySelector('h1');
-    expect(h1.textContent).toContain('About Me');
+
+  it("should render the AbtMeRsmElTitleDirective with default style", () => {
+    let directive: DebugNode = abtMeRsmElTitleDirectiveQueryResponse[0];
+    let el: HTMLElement = directive.nativeNode;
+    expect(el.innerHTML).toContain("About Me");
+    expect(el.classList).toContain("rsm-el-title");
+  });
+
+  it("should show AbtMeRsmElTitleDirective as pressed when touch starts", () => {
+    let directive: DebugNode = abtMeRsmElTitleDirectiveQueryResponse[0];
+    let el: HTMLElement = directive.nativeNode;
+    el.dispatchEvent(new Event("touchstart"));
+    expect(el.classList).toContain("pressed");
+  });
+
+  it("should show AbtMeRsmElTitleDirective as non pressed when touch ends", () => {
+    let directive: DebugNode = abtMeRsmElTitleDirectiveQueryResponse[0];
+    let el: HTMLElement = directive.nativeNode;
+    el.dispatchEvent(new Event("touchstart"));
+    el.dispatchEvent(new Event("touchend"));
+    expect(el.classList).not.toContain("pressed");
   });
 });
